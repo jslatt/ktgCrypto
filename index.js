2893ae9f36718f0cd69aff4ad20ace0c31c29a3d
BlueVein <james@blueve.in>
2021-05-04 14:46:46 -0700

    Social Sentiment, frvol, etc.

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ client.on('message', message => {
 
     // RVol
     if(command === 'rvol' && args[0] != undefined) {
-        message.channel.send('Fetching relative volume for ' + args[0].toUpperCase() + '/USD.')
+        message.channel.send('Fetching relative volume for ' + args[0].toUpperCase() + '.')
         
         const http = require("https");
 
         // 21 Days Before Today YYYY-MM-DD
         dFix = moment().subtract(21, 'days').format('YYYY-MM-DD')
 
-        let path = "/v1/ohlcv/" + args[0].toUpperCase() + "/USD/history?period_id=1DAY&time_start=" + dFix + "T00%3A00%3A00";
+        let path = "/v1/ohlcv/" + args[0].toUpperCase() + "/history?period_id=1DAY&time_start=" + dFix + "T00%3A00%3A00";
 
         const options = {
           "method": "GET",
@@ -70,12 +70,76 @@ client.on('message', message => {
         req.end();
 
     }
+      //Fancy Rvol
+      if(command === 'frvol' && args[0] != undefined) {
+        message.channel.send('Fetching fancy relative volume for ' + args[0].toUpperCase()+".")
+        
+        const http = require("https");
+
+        // 21 Days Before Today YYYY-MM-DD
+        dFix = moment().subtract({hours: 504}).format('YYYY-MM-DD')
+
+        let path = "/v1/ohlcv/" + args[0].toUpperCase() + "/history?period_id=1HRS&time_start=" + dFix + "T00%3A00%3A00";
+
+        const options = {
+          "method": "GET",
+          "hostname": "rest.coinapi.io",
+          "port": null,
+          "path": path,
+          "headers": {
+            "X-CoinAPI-Key": "FDE9F5CF-6150-4E05-90A3-69B59255032B",
+            "Content-Length": "0"
+          }
+        };
+        const req = http.request(options, function (res) {
+          const chunks = [];
+        
+          res.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+        
+          res.on("end", function () {
+            const body = Buffer.concat(chunks);
+            let data = JSON.parse(body);
+            let sum = 0;
+            let counter = 0;
+            //data[0].volume_traded
+            if(Object.values(data).length > 1) {
+              // What hour is it now?
+              let now = moment().format('HH');//Returns 16
+
+              for (i = 0; i < Object.values(data).length; i++){
+                // For each day... get the average volume up to the hour of that day.
+                //message.channel.send("time -" + data[i].time_close.substring(11,13))
+                // If data hour is less than or equal to current requested hour, count. Else - ignore. 
+                if (data[i].time_close.substring(11,13) <= now) {
+                  sum+=data[i].volume_traded;
+                  counter++;
+                }
+              }
+
+              message.channel.send('Total hours qualified ' + counter + ' total volume ' + sum + '`object length - `' + Object.values(data).length)
+            }
+            else {
+              message.channel.send("Coin not supported by API. `Data length of 0`")
+            }
+
+
+          });
+        });
+        
+        req.end();
+
+    }
+    if(command === 'frvol' && args[0] == undefined) {
+      message.channel.send('Error - Need symbol after frvol command. (see ~help)')
+    }
     if(command === 'rvol' && args[0] == undefined) {
       message.channel.send('Error - Need symbol after rvol command. (see ~help)')
     }
     // Help Command
     if(command === 'help') {
-      message.channel.send('**About KTG Crypto Bot (prefix ~)**\nCommands:\n`help` - command info\n`rvol` - gets 21 day average of volume and compares it to prior days (~rvol TICKER/USD)\n`gas` - fetches gas fee prices from https://docs.ethgasstation.info/gas-price#gas-price')
+      message.channel.send('**About KTG Crypto Bot (prefix ~)**\nCommands:\n`help` - command info\n`rvol` - gets 21 day average of volume and compares it to prior days (~rvol TICKER/USD)\n`gas` - fetches gas fee prices from ETH gas station info.\n`social {{command}}` - retrives social sentiment data (LunarCRUSH) and ranks top 10 based on filter. Currently supported: gs - galaxy score, sv -social volume, c - social contibutor mentions (24hrs).')
     }
     // Sleep
     if(command === 'ssheeeesh') {
@@ -150,8 +214,127 @@ client.on('message', message => {
 
 
     }
-    
-})
+    // LunarCRUSH
+    if(command === 'social' && args[0] != undefined) {
+      if (args[0] === "gs") {
+        const http = require("https");
 
-client.login('NzYxMDU4ODI0MTczMTI1NjYz.X3VFQw.JylV4hUkj0KFania0JIeHfo6OLw');
+        const options = {
+          "method": "GET",
+          "hostname": "api.lunarcrush.com",
+          "port": null,
+          "path": "/v2?data=market&key=ege97ydfvyqg024tp5dcqr&limit=10&sort=gs&desc=true",
+          "headers": {
+            "Content-Length": "0"
+          }
+        };
+  
+        const req = http.request(options, function (res) {
+          const chunks = [];
+  
+          res.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+  
+          res.on("end", function () {
+            const body = Buffer.concat(chunks);
+            let d = JSON.parse(body);
+            //Get Symbols d.data[0].s
+            if(Object.values(d).length > 1) {
+              let set = []
+              message.channel.send('Top 10 Galaxy Score Symbols:')
+              for (i = 0; i < 10; i++){
+                set.push(d.data[i].s)
+              }
+              message.channel.send(set.toString())
+            }
+  
+          });
+        });
+  
+        req.end();
+      }
+      if (args[0] === "sv") {
+        const http = require("https");
+
+        const options = {
+          "method": "GET",
+          "hostname": "api.lunarcrush.com",
+          "port": null,
+          "path": "/v2?data=market&key=ege97ydfvyqg024tp5dcqr&limit=10&sort=sv&desc=true",
+          "headers": {
+            "Content-Length": "0"
+          }
+        };
+  
+        const req = http.request(options, function (res) {
+          const chunks = [];
+  
+          res.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+  
+          res.on("end", function () {
+            const body = Buffer.concat(chunks);
+            let d = JSON.parse(body);
+            //Get Symbols d.data[0].s
+            if(Object.values(d).length > 1) {
+              let set = []
+              message.channel.send('Top 10 Social Volume Symbols:')
+              for (i = 0; i < 10; i++){
+                set.push(d.data[i].s)
+              }
+              message.channel.send(set.toString())
+            }
+  
+          });
+        });
+  
+        req.end();
+      }
+      if (args[0] === "c") {
+        const http = require("https");
+
+        const options = {
+          "method": "GET",
+          "hostname": "api.lunarcrush.com",
+          "port": null,
+          "path": "/v2?data=market&key=ege97ydfvyqg024tp5dcqr&limit=10&sort=c&desc=true",
+          "headers": {
+            "Content-Length": "0"
+          }
+        };
+  
+        const req = http.request(options, function (res) {
+          const chunks = [];
+  
+          res.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+  
+          res.on("end", function () {
+            const body = Buffer.concat(chunks);
+            let d = JSON.parse(body);
+            //Get Symbols d.data[0].s
+            if(Object.values(d).length > 1) {
+              let set = []
+              message.channel.send('Top 10 24hr Social Contributor Symbols:')
+              for (i = 0; i < 10; i++){
+                set.push(d.data[i].s)
+              }
+              message.channel.send(set.toString())
+            }
+  
+          });
+        });
+  
+        req.end();
+      }
+
+    }
+})
+//Prod
+//client.login('NzYxMDU4ODI0MTczMTI1NjYz.X3VFQw.JylV4hUkj0KFania0JIeHfo6OLw');
+//Dev
+client.login('ODM4MTk3MjE2MDUyMTE3NTI1.YI3l_Q.MT-sgfwNBVi3myP1lp-adR6Xxrw');
 
